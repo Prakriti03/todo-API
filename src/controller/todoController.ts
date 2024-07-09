717a2463574b5511443c324519191820bfdd35f5
Prakriti <timalsena.prakriti97@gmail.com>
2024-07-09 11:58:36 +0545

    add comments

--- src/controller/todoController.ts
+++ src/controller/todoController.ts
@@ -2,11 +2,32 @@ import { Request, Response } from "express";
 import * as TodoServices from "../services/todoService";
 import { error } from "console";
 
+/**
+ * The function `getTodos` retrieves todos data and sends it as a JSON response.
+ * @param {Request} req - Request object containing information about the HTTP request
+ * @param {Response} res - The `res` parameter in the `getTodos` function is an object representing the
+ * HTTP response that the server sends back to the client. It allows you to send data, set headers, and
+ * manage the response to the client's request.
+ */
 export const getTodos = (req: Request, res: Response) => {
   const data = TodoServices.getTodos();
   res.json(data);
 };
 
+/**
+ * This function retrieves a todo item by its ID from a service and sends it as a response, handling
+ * the case where the todo is not found.
+ * @param {Request} req - The `req` parameter in the function `getTodosById` is of type `Request`,
+ * which typically represents the HTTP request in Express.js or similar frameworks. It contains
+ * information about the incoming request such as headers, parameters, body, etc.
+ * @param {Response} res - The `res` parameter in the function `getTodosById` is an object representing
+ * the HTTP response that the Express application sends when it receives an HTTP request. It allows you
+ * to send data back to the client making the request, such as JSON data or HTML content.
+ * @returns If the `TodoServices.getTodosById(id)` function returns a valid data object for the
+ * specified `id`, then that data object will be returned as a JSON response using `res.json(data)`. If
+ * the `data` variable is empty or falsy, indicating that the todo was not found, then an error message
+ * object `{ error: "Todo not found" }` will be returned as a
+ */
 export const getTodosById = (req: Request, res: Response) => {
   const { id } = req.params;
   const data = TodoServices.getTodosById(id);
@@ -19,6 +40,17 @@ export const getTodosById = (req: Request, res: Response) => {
   res.json(data);
 };
 
+/**
+ * The function `addTodo` receives a request, validates a todo object, sets a default value for
+ * `completed`, adds the todo using `TodoServices`, and sends back the result in the response.
+ * @param {Request} req - Request object containing information about the HTTP request
+ * @param {Response} res - The `res` parameter in the `addTodo` function is an object representing the
+ * HTTP response that the server sends back to the client. It is of type `Response`, which is typically
+ * provided by a web framework like Express in Node.js. The `res` object has methods like `json()`
+ * @returns The function `addTodo` is returning the data object that is the result of adding the todo
+ * item using `TodoServices.addTodo(todo)`. This data object is then sent as a JSON response using
+ * `res.json(data)`.
+ */
 export const addTodo = (req: Request, res: Response) => {
   const todo = req.body;
 
@@ -34,6 +66,16 @@ export const addTodo = (req: Request, res: Response) => {
   const data = TodoServices.addTodo(todo);
   res.json(data);
 };
+
+/**
+ * This function deletes a todo item based on the provided id and returns the updated data.
+ * @param {Request} req - The `req` parameter in the `deleteTodo` function is of type `Request`, which
+ * typically represents the HTTP request in Express.js or similar frameworks. It contains information
+ * about the incoming request such as headers, parameters, body, etc.
+ * @param {Response} res - The `res` parameter in the `deleteTodo` function is an object representing
+ * the HTTP response that the server sends back to the client. It is used to send data back to the
+ * client in the form of JSON, HTML, or other formats. In this case, the `res.json(data)`
+ */
 export const deleteTodo = (req: Request, res: Response) => {
   const { id } = req.params;
   const data = TodoServices.deleteTodo(id);
@@ -41,6 +83,14 @@ export const deleteTodo = (req: Request, res: Response) => {
   res.json(data);
 };
 
+/**
+ * The function `updateTodo` updates a todo item based on the provided id and request body.
+ * @param {Request} req - Request object containing information about the HTTP request
+ * @param {Response} res - The `res` parameter in the `updateTodo` function is an instance of the
+ * Express Response object. It is used to send a response back to the client making the request. In
+ * this function, `res.json()` is used to send a JSON response containing the updated todo data back to
+ * the client
+ */
 export const updateTodo = (req: Request, res: Response) => {
   const { id } = req.params;
   if (!TodoServices.getTodosById(id)) {
--- src/model/todos.ts
+++ src/model/todos.ts
@@ -17,11 +17,26 @@ export const getTodos = (): Todo[] => {
   return todos;
 };
 
+/**
+ * The function `getTodosById` retrieves a todo item by its ID from a list of todos.
+ * @param {string} id - The `id` parameter in the `getTodosById` function is a string that represents
+ * the unique identifier of a todo item. The function searches for a todo item in the `todos` array
+ * based on this `id` and returns the matching todo item data.
+ * @returns The function `getTodosById` is returning the todo item with the specified `id` from the
+ * `todos` array.
+ */
 export const getTodosById = (id: string) => {
   const data = todos.find(({ id: todoId }) => todoId === id);
   return data;
 };
 
+/**
+ * The `addTodo` function adds a new todo item to a list of todos with a unique ID.
+ * @param todo - The `todo` parameter is an object that represents a new todo item to be added to a
+ * list of todos. It is of type `Omit<Todo, "id">`, which means it includes all properties of the
+ * `Todo` type except for the `id` property.
+ * @returns The function `addTodo` is returning the updated `todos` array after adding a new todo item.
+ */
 export const addTodo = (todo: Omit<Todo, "id">) => {
   todos.push({
     id: `${todos.length + 1}`,
@@ -30,11 +45,30 @@ export const addTodo = (todo: Omit<Todo, "id">) => {
   return todos;
 };
 
+/**
+ * The `deleteTodo` function removes a todo item with a specific id from an array of todos and returns
+ * the updated array.
+ * @param {string} id - The `id` parameter in the `deleteTodo` function is a string representing the
+ * unique identifier of the todo item that needs to be deleted from the list of todos.
+ * @returns The `deleteTodo` function is returning an array of `Todo` objects after filtering out the
+ * todo with the specified `id`.
+ */
 export const deleteTodo = (id: string): Todo[] => {
   todos = todos.filter((todo) => todo.id !== id);
   return todos;
 };
 
+/**
+ * The function `updateTodo` updates a specific todo item in a list of todos based on its id.
+ * @param {string} id - The `id` parameter in the `updateTodo` function is a string that represents the
+ * unique identifier of the todo item that needs to be updated.
+ * @param {Todo} todo - The `todo` parameter in the `updateTodo` function represents the updated
+ * information for a specific todo item. It typically includes properties such as `title`,
+ * `description`, `dueDate`, `priority`, etc., depending on the structure of your `Todo` type. This
+ * parameter is used to update
+ * @returns The function `updateTodo` is returning the updated todo item after updating it with the new
+ * values provided.
+ */
 export const updateTodo = (id: string, todo: Todo): Todo => {
   let todoToUpdate = getTodosById(id);
 
